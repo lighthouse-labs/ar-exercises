b34b0c7b0462fc6d7083d0629ac9eb323c63249a
Khurram Virani <kvirani@gmail.com>
2014-03-19 09:15:13 -0700

    logging

--- setup.rb
+++ setup.rb
@@ -2,47 +2,42 @@ require 'active_record'
 require_relative 'lib/store'
 require_relative 'lib/employee'
 
-print "Establishing connection to database ..."
+# Output messages from AR to STDOUT
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+puts "Establishing connection to database ..."
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
   encoding: 'unicode',
   pool: 5,
   database: 'dd0g8jjf5k806s',
   username: 'awidipxgjksoce',
-  password: 'xxxxx',
+  password: 'xxx',
   host: 'ec2-174-129-21-42.compute-1.amazonaws.com',
-  port: 5432
+  port: 5432,
+  min_messages: 'error'
 )
 puts "CONNECTED"
 
-print "Setting up Database (if needed) ..."
+print "Setting up Database (recreating tables) ..."
 
 ActiveRecord::Schema.define do
-  unless ActiveRecord::Base.connection.table_exists? 'stores'
-    create_table :stores do |table|
-      table.column :name, :string
-      table.column :annual_revenue, :string
-      table.column :mens_apparel, :boolean
-      table.column :womens_apparel, :boolean
-      table.timestamps
-    end
+  drop_table :stores
+  drop_table :employees
+  create_table :stores do |table|
+    table.column :name, :string
+    table.column :annual_revenue, :string
+    table.column :mens_apparel, :boolean
+    table.column :womens_apparel, :boolean
+    table.timestamps
   end
-  unless ActiveRecord::Base.connection.table_exists? 'employees'
-    create_table :employees do |table|
-      table.references :store
-      table.column :first_name, :string
-      table.column :last_name, :string
-      table.column :hourly_rate, :integer
-      table.timestamps
-    end
+  create_table :employees do |table|
+    table.references :store
+    table.column :first_name, :string
+    table.column :last_name, :string
+    table.column :hourly_rate, :integer
+    table.timestamps
   end
 end
 
-puts "DONE"
-
-print "Clearing Database (start fresh everytime) ..."
-Store.delete_all
-Employee.delete_all
-ActiveRecord::Base.connection.execute("ALTER SEQUENCE stores_id_seq RESTART WITH 1;")
-ActiveRecord::Base.connection.execute("ALTER SEQUENCE employees_id_seq RESTART WITH 1;")
 puts "DONE"
