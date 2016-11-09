cbc560e1ad1b5a94a4478506de79c4ed7de6a838
Brett Goss <brettgoss@live.com>
2016-11-08 17:58:43 -0800

    ex1 completed

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true);
+store = Store.create(name: 'Richmond', annual_revenue: 126000, mens_apparel: true)
+store = Store.create(name: 'Gastown', annual_revenue: 190000, womens_apparel: true)
+store.save
+puts Store.count
