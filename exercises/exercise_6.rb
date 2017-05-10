74ce1ec31f0a4e2da3f0968fc4d50c04de977a31
Elvis Chan <choectt@gmail.com>
2017-05-10 01:58:04 +0000

    Completed all exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,18 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# Add some employees to stores
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Sam", last_name: "Don", hourly_rate: 40)
+@store2.employees.create(first_name: "Angie", last_name: "Wade", hourly_rate: 50)
+@store3.employees.create(first_name: "Kit", last_name: "Muni", hourly_rate: 40)
+@store3.employees.create(first_name: "Pat", last_name: "Vasi", hourly_rate: 55)
+@store4.employees.create(first_name: "Venus", last_name: "Pope", hourly_rate: 43)
+@store5.employees.create(first_name: "Hughes", last_name: "Ivans", hourly_rate: 55)
+@store5.employees.create(first_name: "John", last_name: "Dope", hourly_rate: 48)
+@store6.employees.create(first_name: "Pete", last_name: "Russell", hourly_rate: 60)
+
+# example to output errors for store5 when creating employees
+puts @store5.employees.map { |e| e.errors.inspect }
+# example to reload databse
+puts @store5.reload.employees
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,46 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# To test out validation errors
+
+# Stores Creation Attempt (should not be added)
+@store7 = Store.create(name: "London")
+
+# if errors, throw error message immediately
+# @store7 = Store.create!(name: "London")
+
+# false if store7 is not saved
+puts "Is '@store7' Persisted? #{@store7.persisted?}"
+
+# output store7 name only when there's no errors
+if @store7.errors.any?
+  puts "\n@store7 errors:"
+  puts @store7.errors.to_a
+else
+  puts "@store7 names:"
+  puts @store7.name
+end
+
+# output all store names for verfication
+puts "\nAll stores:"
+puts Store.pluck(:name).to_a
+# same as puts Store.all.maps(&:name).to_a
+
+puts "\nBefore Creating New Employees"
+puts @store5.employees.each { |employee| puts "Store 5: #{employee.first_name} #{employee.last_name}"}
+puts @store6.employees.each { |employee| puts "Store 6: #{employee.first_name} #{employee.last_name}"}
+# Employees Creation (should not be added)
+employee1 = @store5.employees.create(first_name: "Another", last_name: "Guy", hourly_rate: 30)
+employee2 = @store6.employees.create(last_name: "Gal", hourly_rate: 50)
+
+# Verification
+puts "\nAfter Attempt to Create New Employees"
+puts "Is 'employee1' Persisted? #{employee1.persisted?}"
+puts "Is 'employee2' Persisted? #{employee2.persisted?}"
+
+puts @store5.reload.employees.each { |employee| puts "Store 5 Employees: #{employee.first_name} #{employee.last_name}" }
+# alternate way to display directly from DB
+Employee.where(store_id: @store5.id).each { |employee| puts "Store 5 Employees: #{employee.first_name} #{employee.last_name}"}
+# puts Employee.where(store_id: @store5.id).pluck(:first_name)
+
+puts @store6.reload.employees.each { |employee| puts "Store 6 Employees: #{employee.first_name} #{employee.last_name}" }
