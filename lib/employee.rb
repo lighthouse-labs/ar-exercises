0b11b508df639ea0397b44a16b79f048b134d8da
Elvis Chan <choectt@gmail.com>
2017-05-10 05:35:14 +0000

    Employees will be assigned with a random password

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,7 @@
 class Employee < ActiveRecord::Base
+  # automatically generate and assign password upon Employee creation
+  before_create :gen_password
+
   belongs_to :store
   validates :first_name, :last_name,
             presence: true
@@ -6,4 +9,9 @@ class Employee < ActiveRecord::Base
             numericality: { only_integer: true,
                             greater_than_or_equal_to: 40,
                             less_than_or_equal_to: 200 }
+  private
+    def gen_password
+      self.password = ([*('A'..'Z'),*('a'..'z'),*('0'..'9')]-%w(0 1 I O)).sample(8).join
+    end
+
 end
