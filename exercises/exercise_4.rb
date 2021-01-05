ef6269d0ad210dbf837b99900208e1b4ea17b7ae
EagerBeaver98 <jooke98@outlook.com>
2021-01-05 05:08:09 +0000

    exercise 4 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,15 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(:name => 'Surrey', :annual_revenue => 224000, :mens_apparel => false, :womens_apparel => true)
+
+Store.create(:name => 'Whistler', :annual_revenue => 1900000, :mens_apparel => true, :womens_apparel => false)
+
+Store.create(:name => 'Yaletown', :annual_revenue => 190000, :mens_apparel => true, :womens_apparel => true)
+
+@mens_apparel = Store.where("mens_apparel = true")
+
+
+@mens_apparel.each { |x| puts x.name }
+
+@under1mil = Store.where(["womens_apparel = ? and annual_revenue < ?", "true", "000000"])
