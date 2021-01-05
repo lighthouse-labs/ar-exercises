67e3dc625f4c32146b1e69657265ca75389c8bf2
EagerBeaver98 <jooke98@outlook.com>
2021-01-05 05:22:48 +0000

    exercise 5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@company_total = Store.sum("annual_revenue")
+puts @company_total
+@all = Store.where("id > 0")
+puts @company_total / @all.length
+@over1mil = Store.where("annual_revenue > 1000000")
+puts @over1mil.length
