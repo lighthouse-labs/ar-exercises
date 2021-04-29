a188f45bafb72e860bc7a8f5a2534237fc8ade67
Josiah <josiah.pilon@yahoo.ca>
2021-04-28 22:08:12 -0700

    Returns the sum and average of annual_revenue for the stores. Returns the stores with annual_revenue over $1M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts Store.sum(:annual_revenue)
+puts Store.average(:annual_revenue)
+puts Store.where("annual_revenue > 1000000").count
+ 
