aae6d7d9446538aa56a1bed28f93628e28ab8bd9
Josiah <josiah.pilon@yahoo.ca>
2021-04-28 22:10:30 -0700

    Validates for users input. Displays errors if validations are false

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+print "Pleas input a store name: "
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+new_store.valid?
+puts "Error messages*****"
+puts new_store.errors.full_messages
