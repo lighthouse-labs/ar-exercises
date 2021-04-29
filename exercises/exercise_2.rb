c6883279e31844d91c273f27197a783dba4fd018
Josiah <josiah.pilon@yahoo.ca>
2021-04-28 18:31:36 -0700

    Retrieves the first and second rows of the stores table, into variables
    Changes the first rows name in the stores table

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "langley"
+@store1.save
+p @store1.name
