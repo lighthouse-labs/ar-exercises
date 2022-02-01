98d74a5e6813c565a08e00b4a7daa36b59c93a3e
cdmac94 <collinmacleod79@gmail.com>
2022-02-01 06:07:21 +0000

    Added three stores and functions to list all stores within parameters

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,28 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey",
+            annual_revenue: 224000,
+            mens_apparel: false,
+            womens_apparel: true
+            )
+Store.create(name: "Whistler",
+            annual_revenue: 1900000,
+            mens_apparel: true,
+            womens_apparel: false
+            )
+Store.create(name: "Yaletown",
+            annual_revenue: 430000,
+            mens_apparel: true,
+            womens_apparel: false
+            )
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts "Name: #{store.name} Annual Revenue:#{store.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = true and annual_revenue < 1000000")
+@womens_stores.each do |store|
+  puts "Name: #{store.name} Annual Revenue:#{store.annual_revenue}"
+end
